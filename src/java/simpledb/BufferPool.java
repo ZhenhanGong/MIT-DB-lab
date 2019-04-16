782e2c629c9090b5018ba5f81a62105a668ab9ee
zhenhan gong <zhenhan.gong@gmail.com>
2019-04-16 22:29:27 +0800

    finish ex3

--- src/java/simpledb/BufferPool.java
+++ src/java/simpledb/BufferPool.java
@@ -26,6 +26,8 @@ public class BufferPool {
     constructor instead. */
     public static final int DEFAULT_PAGES = 50;
 
+    private static ConcurrentHashMap<PageId, Page> pages;
+
     /**
      * Creates a BufferPool that caches up to numPages pages.
      *
@@ -33,6 +35,7 @@ public class BufferPool {
      */
     public BufferPool(int numPages) {
         // some code goes here
+        pages = new ConcurrentHashMap<>();
     }
     
     public static int getPageSize() {
@@ -67,7 +70,7 @@ public class BufferPool {
     public  Page getPage(TransactionId tid, PageId pid, Permissions perm)
         throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        return pages.get(pid);
     }
 
     /**
