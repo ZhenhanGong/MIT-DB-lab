c089b6f786c9e0ca2ae60b5a0004295e397a93ce
zhenhan gong <zhenhan.gong@gmail.com>
2019-05-22 18:53:08 +0800

    add script to generate .dat file

---
+++ generate_dat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if [ $1 == "--help" ]
+then
+    printf "./generate_dat.sh ${GREEN} convert file.txt  N ${NC} to convert file.txt to file.dat (N = #colums) \n"
+    printf "./generate_dat.sh ${GREEN} print   file.dat  N ${NC} to print table layout (N = #colums) \n"
+fi
+java -jar dist/simpledb.jar $1 $2 $3
