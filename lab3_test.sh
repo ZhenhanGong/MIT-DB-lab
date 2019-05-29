864bf34e4b5cd1c9de9fc57331b3c791fd153e63
zhenhan gong <zhenhan.gong@gmail.com>
2019-05-29 09:29:01 +0800

    add all test to lab3_test.sh

--- lab3_test.sh
+++ lab3_test.sh
@@ -4,7 +4,7 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 COLOR=${RED}
-for TESTNAME in IntHistogramTest
+for TESTNAME in IntHistogramTest TableStatsTest JoinOptimizerTest
 do
     ant runtest -Dtest=${TESTNAME}
     if [ $? == 0 ]
@@ -17,15 +17,15 @@ do
     printf "${COLOR} ---------------------------------------------------------------------${NC}\n"
 done
 
-# for TESTNAME in ScanTest
-# do
-#     ant runsystest -Dtest=${TESTNAME}
-#     if [ $? == 0 ]
-#     then
-#         COLOR=${GREEN}
-#     else
-#         COLOR=${RED}
-#     fi
-#     printf "${COLOR} ${TESTNAME} system test result\n"
-#     printf "${COLOR} ---------------------------------------------------------------------${NC}\n"
-# done
+for TESTNAME in QueryTest
+do
+    ant runsystest -Dtest=${TESTNAME}
+    if [ $? == 0 ]
+    then
+        COLOR=${GREEN}
+    else
+        COLOR=${RED}
+    fi
+    printf "${COLOR} ${TESTNAME} system test result\n"
+    printf "${COLOR} ---------------------------------------------------------------------${NC}\n"
+done
