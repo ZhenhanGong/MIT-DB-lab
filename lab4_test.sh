b9c47aab9a47b8b4f3a9edd76caec9f1af45319b
zhenhan gong <zhenhan.gong@gmail.com>
2019-05-31 23:53:55 +0800

    add script to test lab4

---
+++ lab4_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+COLOR=${RED}
+for TESTNAME in LockingTest TransactionTest DeadlockTest
+do
+    ant runtest -Dtest=${TESTNAME}
+    if [ $? == 0 ]
+    then
+        COLOR=${GREEN}
+    else
+        COLOR=${RED}
+    fi
+    printf "${COLOR} ${TESTNAME} unit test result\n"
+    printf "${COLOR} ---------------------------------------------------------------------${NC}\n"
+done
+
+for TESTNAME in AbortEvictionTest TransactionTest
+do
+    ant runsystest -Dtest=${TESTNAME}
+    if [ $? == 0 ]
+    then
+        COLOR=${GREEN}
+    else
+        COLOR=${RED}
+    fi
+    printf "${COLOR} ${TESTNAME} system test result\n"
+    printf "${COLOR} ---------------------------------------------------------------------${NC}\n"
+done
